0444949e4899d85fde56a5e05634899997affed8
Muhammad Zachrie Kurniawan <zachrie2005@gmail.com>
2024-05-21 16:59:11 +0700

    update tugas satu

---
+++ tugas/tugas_satu.php
@@ -0,0 +1,7 @@
+<?php
+echo "Nama: Muhammad Zachrie Kurniawan <br>"
+  . "Asal Kampus: Universitas Lampung <br>"
+  . "Jurusan: Teknik Informatika <br>"
+  . "Kota: Bandar Lampung <br>"
+  . "PHP: Pernah <br>"
+  . "HTML: Pernah <br>";
