10b31fb8f75b11c5b62bbfe16f473847bbe08831
Muhammad Zachrie Kurniawan <zachrie2005@gmail.com>
2024-05-28 21:06:30 +0700

    feat: add tugas_bmi

--- tugas/proses_hitung.php
+++ tugas/proses_hitung.php
@@ -1,26 +1,30 @@
 <?php
-// mengambil data dari method POST
-$nama = $_POST['nama'];
-$berat = $_POST['berat'];
-$tinggi = $_POST['tinggi'];
 
-$bmi = $berat / ($tinggi * $tinggi);
-$bmi_msg = "Nilai BMI Anda Adalah : ";
+if (isset($_POST['nama']) && isset($_POST['berat']) && isset($_POST['tinggi'])) {
+  // mengambil data dari method POST
+  $nama = $_POST['nama'];
+  $berat = $_POST['berat'];
+  $tinggi = $_POST['tinggi'];
 
-echo $nama . "<br>";
-echo $bmi_msg . $bmi . "<br>";
-get_grade($bmi);
+  $nilai_bmi = $berat / ($tinggi * $tinggi);
+  $bmi_msg = "Nilai BMI Anda Adalah : ";
 
-function get_grade($bmi)
-{
-  $status_msg = "Status BMI : ";
-  if ($bmi > 30.0) {
-    echo $status_msg . "Obesitas <br>";
-  } else if ($bmi > 25.0) {
-    echo $status_msg . "Kegemukan <br>";
-  } else if ($bmi > 18.5) {
-    echo $status_msg . "Normal";
-  } else {
-    echo $status_msg . "Kurus";
+  echo $nama . "<br>";
+  echo $bmi_msg . $nilai_bmi . "<br>";
+
+  function get_grade($bmi)
+  {
+    $status_msg = "Status BMI : ";
+    if ($bmi > 30.0) {
+      echo $status_msg . "Obesitas <br>";
+    } else if ($bmi > 25.0) {
+      echo $status_msg . "Kegemukan <br>";
+    } else if ($bmi > 18.5) {
+      echo $status_msg . "Normal";
+    } else {
+      echo $status_msg . "Kurus";
+    }
   }
+
+  get_grade($nilai_bmi);
 }
